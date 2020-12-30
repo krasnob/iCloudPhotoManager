441dff21edab3efbeaedd4339d3f639b79116e55
Sasha <okrasnobaiev@bloomberg.net>
2020-12-30 00:29:50 -0500

    Initial Commit

---
+++ Shared/iCloudPhotoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudPhotoManagerApp.swift
+//  Shared
+//
+//  Created by Oleksandr Krasnobaiev on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct iCloudPhotoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
